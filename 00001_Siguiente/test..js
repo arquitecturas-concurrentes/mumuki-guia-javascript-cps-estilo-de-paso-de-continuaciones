d7f514b2e89e2afc9b72337cbe5103181b39bbdc
franco@mumuki.org <franco@mumuki.org>
2018-08-28 15:54:21 -0400

    Mumuki Export on 2018-08-28 15:54:21 -0400

--- 00001_Siguiente/test..js
+++ 00001_Siguiente/test..js
@@ -1,9 +1,9 @@
 describe("successor", function() {
   it("succcesor(1) == 2", function() {
-    assert.equal(succesor(1), 2)
+    assert.equal(successor(1), 2)
   })
   
   it("succcesor(30) == 31", function() {
-    assert.equal(succesor(30), 31)
+    assert.equal(successor(30), 31)
   })
 })
